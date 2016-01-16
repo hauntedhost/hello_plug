1e31b42bee7c99e9a8364542e5ac29c18a806821
Sean Omlor <somlor@eml.cc>
2016-01-16 01:23:06 -0500

    simple router

--- README.md
+++ README.md
@@ -1,20 +1,9 @@
 # HelloPlug
 
-**TODO: Add description**
+Tinkering with Elixir [Plug](https://github.com/elixir-lang/plug).
 
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
-
-  1. Add hello_plug to your list of dependencies in `mix.exs`:
-
-        def deps do
-          [{:hello_plug, "~> 0.0.1"}]
-        end
-
-  2. Ensure hello_plug is started before your application:
-
-        def application do
-          [applications: [:hello_plug]]
-        end
-
+```
+$ iex -S mix
+iex(1)> {:ok, _} = Plug.Adapters.Cowboy.http(HelloPlug, [])
+open http://localhost:4000/hello
+```
--- lib/hello_plug.ex
+++ lib/hello_plug.ex
@@ -5,9 +5,25 @@ defmodule HelloPlug do
   end
 
   def call(conn, _opts) do
-    IO.puts("Saying Hello!")
-    conn
-    |> Plug.Conn.put_resp_header("Server", "Plug")
-    |> Plug.Conn.send_resp(200, "Hello, world!")
+    IO.puts("Passing to router ...")
+    route(conn.method, conn.path_info, conn |> with_headers)
+  end
+
+  def route("GET", ["hello"], conn) do
+    IO.puts("/hello")
+    conn |> Plug.Conn.send_resp(200, "Hello, world!")
+  end
+
+  def route("GET", ["cats", cat_id], conn) do
+    IO.puts("/cat/#{cat_id}")
+    conn |> Plug.Conn.send_resp(200, "You requested cat #{cat_id}")
+  end
+
+  def route(_, _, conn) do
+    conn |> Plug.Conn.send_resp(404, "Not found.")
+  end
+
+  defp with_headers(conn) do
+    conn |> Plug.Conn.put_resp_header("Server", "Plug")
   end
 end
